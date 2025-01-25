86b64b84d4a2af1ccda19d90fb3362d0e3d2ffe9
LeoSilva234 <Leonardodasilv3211@gmail.com>
2025-01-24 21:51:26 -0300

    Estudar no sabado

--- js/config.js
+++ js/config.js
@@ -1,4 +1,17 @@
 /**
+ * Chaves de conexão ccom o Firebase.
+ * ATENÇÃO! Sempre troque essas chaves pelas chaves do seu projeto.
+ **/
+const firebaseConfig = {
+  apiKey: "AIzaSyDdHbHUxuAzSwMJkLZYqrsgAbuN5SeoAj0",
+  authDomain: "meu-blog-estatico-c96ef.firebaseapp.com",
+  projectId: "meu-blog-estatico-c96ef",
+  storageBucket: "meu-blog-estatico-c96ef.firebasestorage.app",
+  messagingSenderId: "176305610540",
+  appId: "1:176305610540:web:e9c5a14c2f6fe284625689"
+};
+
+/**
  * Configuração do site;
  **/
 const site = {
